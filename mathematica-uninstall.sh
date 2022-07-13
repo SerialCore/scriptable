9db6ab477ae7719624930676189c914ad2457b1b
SerialCore <serialcore@outlook.com>
2022-07-13 07:28:32 -0400

    add mathematica-uninstall.sh

---
+++ mathematica-uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo rm -r /usr/local/Wolfram/Mathematica/
+
+sudo rm -r /usr/share/Mathematica/
+
+rm -r /home/$USER/.Mathematica/
+
+rm -r /home/$USER/.Wolfram/
+
+rm -r /home/$USER/.cache/Wolfram/
+
+rm -r "/home/$USER/Documents/Wolfram Mathematica"
